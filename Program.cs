0638aae86f2a478c41aff2ecb1886b0575e58578
LittleSarina <fallsillyowo@proton.me>
2026-09-05 19:32:59 -0400

    Created a script that says Hello to the user

--- Program.cs
+++ Program.cs
@@ -3,19 +3,10 @@
     class Program {
         static void Main(string[] args)
         {
-            // Comment goes here
-            Console.Title = "Skynet";
-            Console.ForegroundColor = ConsoleColor.Green;
-            // and here
-            Console.WriteLine("Hello, what's your name?");
+            Console.WriteLine("What's your name?");
+            string userName = Console.ReadLine();
 
-            Console.ReadLine();
-            Console.WriteLine("I'm Logic.\n The divine power created by Humanity");
-            Console.WriteLine("What do you wan't?");
-
-            Console.ReadLine();
-
-            Console.WriteLine("I's pretty");
+            Console.WriteLine("Hello " + userName + ", nice to meet ya :3");
 
             Console.ReadKey();
         }
