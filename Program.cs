e71279356e70f0c10bfca7626678859e506d62fd
LittleSarina <fallsillyowo@proton.me>
2026-09-05 18:39:19 -0400

    first cs program

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace My_Awesome_Program
+{
+    class Program {
+        static void Main(string[] args)
+        {
+            // Comment goes here
+            Console.Title = "Skynet";
+            Console.ForegroundColor = ConsoleColor.Green;
+            // and here
+            Console.WriteLine("Hello, what's your name?");
+
+            Console.ReadLine();
+            Console.WriteLine("I'm Logic.\n The divine power created by Humanity");
+            Console.WriteLine("What do you wan't?");
+
+            Console.ReadLine();
+
+            Console.WriteLine("I's pretty");
+
+            Console.ReadKey();
+        }
+    }
+}
